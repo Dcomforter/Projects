6dfd0fe16c627a84aa65a6c7c23d98d9c55ebe4d
Dcomforter <104809559+Dcomforter@users.noreply.github.com>
2023-10-17 10:15:39 -0500

    Updated comments

--- black_friday.py
+++ black_friday.py
@@ -2,13 +2,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-# from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LinearRegression
-# from sklearn.tree import DecisionTreeRegressor
-# from sklearn.ensemble import RandomForestRegressor
-# from xgboost.sklearn import XGBRegressor
-# from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-# from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from xgboost.sklearn import XGBRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.preprocessing import LabelEncoder
 
 <<<<<<< HEAD
 =======
